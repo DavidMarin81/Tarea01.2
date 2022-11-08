3d44eb43ee7a849fe2a4c82357be58cddad4f8da
a20davidmg <davidmaringomez81@gmail.com>
2022-11-08 21:06:54 +0100

    Tarea 01.1 - Version 1.0

---
+++ src/es/teis/edu/DOMXMLService.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.teis.edu;
+
+import es.teis.data.exceptions.LecturaException;
+import es.teis.dataXML.IXMLService;
+import es.teis.model.Partido;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author a20davidmg
+ */
+public class DOMXMLService implements IXMLService {
+
+    @Override
+    public ArrayList<Partido> leerPartidos(String ruta, float umbral) throws LecturaException {
+        
+       long id = 0L;
+       String nombre = "";
+       int votos = 0;
+       float porcentaje = 0f;
+       
+       Partido partido = null;
+       ArrayList<Partido> partidos = new ArrayList<>();
+        
+        try {
+            File inputFile = new File("src\\docs\\elecciones.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            
+            doc.getDocumentElement().normalize();
+            
+            NodeList nList = doc.getElementsByTagName(IXMLService.PARTIDO_TAG);
+            for (int i = 0; i < nList.getLength(); i++) {
+                Node nNode = nList.item(i);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    id = Long.valueOf(eElement.getAttribute("id"));
+                    nombre = eElement.getElementsByTagName("nombre").item(0).getTextContent();
+                    votos = Integer.parseInt(eElement.getElementsByTagName("votos_numero").item(0).getTextContent());
+                    porcentaje = Float.parseFloat(eElement.getElementsByTagName("votos_porciento").item(0).getTextContent());
+                }
+                
+                partido = new Partido(id, nombre, votos, porcentaje);
+                partidos.add(partido);
+            }
+            
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(DOMXMLService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(DOMXMLService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(DOMXMLService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
