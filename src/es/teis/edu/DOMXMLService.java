889c7e4e156663d0af09ed4906745ca515f3e3e4
a20davidmg <davidmaringomez81@gmail.com>
2022-11-08 21:16:27 +0100

    Tarea 01.1 - Version 1.2

--- src/es/teis/edu/DOMXMLService.java
+++ src/es/teis/edu/DOMXMLService.java
@@ -29,41 +29,47 @@ public class DOMXMLService implements IXMLService {
 
     @Override
     public ArrayList<Partido> leerPartidos(String ruta, float umbral) throws LecturaException {
-        
-       long id = 0L;
-       String nombre = "";
-       int votos = 0;
-       float porcentaje = 0f;
-       
-       Partido partido = null;
-       ArrayList<Partido> partidos = new ArrayList<>();
-        
+
+        long id = 0L;
+        String nombre = "";
+        int votos = 0;
+        float porcentaje = 0f;
+
+        Partido partido = null;
+        ArrayList<Partido> partidos = new ArrayList<>();
+
         try {
             File inputFile = new File("src\\docs\\elecciones.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(inputFile);
-            
+
             doc.getDocumentElement().normalize();
-            
+
             NodeList nList = doc.getElementsByTagName(IXMLService.PARTIDO_TAG);
             for (int i = 0; i < nList.getLength(); i++) {
                 Node nNode = nList.item(i);
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
-                    id = Long.valueOf(eElement.getAttribute("id"));
-                    nombre = eElement.getElementsByTagName("nombre").item(0).getTextContent();
-                    votos = Integer.parseInt(eElement.getElementsByTagName("votos_numero").item(0).getTextContent());
+
                     porcentaje = Float.parseFloat(eElement.getElementsByTagName("votos_porciento").item(0).getTextContent());
+                    
+                    if (porcentaje > umbral) {
+                        id = Long.valueOf(eElement.getAttribute("id"));
+                        nombre = eElement.getElementsByTagName("nombre").item(0).getTextContent();
+                        votos = Integer.parseInt(eElement.getElementsByTagName("votos_numero").item(0).getTextContent());
+                        
+                        partido = new Partido(id, nombre, votos, porcentaje);
+                        partidos.add(partido);
+                    }
+
                 }
-                
-                partido = new Partido(id, nombre, votos, porcentaje);
-                partidos.add(partido);
+
             }
-            
+
         } catch (Exception ex) {
             throw new LecturaException("Error", ruta);
-        } 
+        }
 
         return partidos;
     }
